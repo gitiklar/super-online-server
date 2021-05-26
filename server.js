f3f201350293be48cc310ea83527a20b4c257de9
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-05-26 20:25:51 +0300

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const mongoose = require('mongoose');
 
